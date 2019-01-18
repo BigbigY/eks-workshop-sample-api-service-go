f36c60242a43050fe790d8de66fdf04835ce9649
Luck <BigbigY@users.noreply.github.com>
2019-01-18 11:22:23 +0800

    pipline test

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World WangYangYang"}
+		res := &response{Message: "Hello World Golang"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
